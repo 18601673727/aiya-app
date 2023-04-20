3a3098f6dcb33b9302d757af6983a7ab747c7008
18601673727 <18601673727@163.com>
2023-04-20 17:36:30 +0800

    handled hot and cold android intent

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,67 @@
-import 'dart:async';
-import 'dart:convert';
-
+import 'package:fl_shared_link/fl_shared_link.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:power_file_view/power_file_view.dart';
-import 'package:aiya/power_file_view_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-import 'permission_util.dart';
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
 
-void main() async {
+// final androidIntentProvider = FutureProvider<AndroidIntentModel?>((ref) async {
+//   return await FlSharedLink().intentWithAndroid;
+// });
+
+// final onAndroidIntentProvider = FutureProvider<String?>((ref) {
+//   return;
+// });
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  PowerFileViewManager.initLogEnable(true, true);
-  PowerFileViewManager.initEngine();
-  runApp(const MyApp());
+
+  final container = ProviderContainer();
+
+  // 冷启动时把intent写入
+  final intent = await FlSharedLink().intentWithAndroid;
+  container.read(androidIntentControllerProvider).intent = intent;
+
+  runApp(UncontrolledProviderScope(
+    container: container,
+    child: const MyApp(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AndroidIntentController extends ChangeNotifier {
+  AndroidIntentModel? intent;
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
+  Future<void> update(data) async {
+    intent = data;
+    notifyListeners();
   }
 }
 
-const List<String> files = [
-  "https://www.xiangmin.net/downloads/test.pdf",
-  "https://www.xiangmin.net/downloads/test.docx",
-  "https://www.xiangmin.net/downloads/test.doc",
-  "https://www.xiangmin.net/downloads/test.xlsx",
-  "https://www.xiangmin.net/downloads/test.xls",
-  "https://www.xiangmin.net/downloads/test.pptx",
-  "https://www.xiangmin.net/downloads/test.ppt",
-  "https://www.xiangmin.net/downloads/test.txt",
-];
+final androidIntentControllerProvider = ChangeNotifierProvider<AndroidIntentController>((ref) {
+  return AndroidIntentController();
+});
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('文件列表'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.watch(androidIntentControllerProvider);
+
+    FlSharedLink().receiveHandler(onIntent: (AndroidIntentModel? data) {
+      logger.i("接收到来自其他应用的intent!");
+      ref.read(androidIntentControllerProvider).update(data);
+    });
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(
+          child: Text(controller.intent?.id ?? ""),
+        ),
       ),
-      body: ListView.builder(
-          itemCount: files.length,
-          itemBuilder: (context, index) {
-            String filePath = files[index];
-            final fileName = FileUtil.getFileName(filePath);
-            final fileType = FileUtil.getFileType(filePath);
-            return Container(
-              margin: const EdgeInsets.only(top: 10.0),
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: ElevatedButton(
-                onPressed: () async {
-                  String savePath = await getFilePath(fileType, fileName);
-                  if (context.mounted) {
-                    onTap(context, filePath, savePath);
-                  }
-                },
-                child: Text(fileName),
-              ),
-            );
-          }),
     );
-  }
-
-  Future onTap(BuildContext context, String downloadUrl, String downloadPath) async {
-    bool isGranted = await PermissionUtil.check();
-
-    if (isGranted && context.mounted) {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (ctx) {
-          return PowerFileViewPage(
-            downloadUrl: downloadUrl,
-            downloadPath: downloadPath,
-          );
-        }),
-      );
-    } else {
-      debugPrint('no permission');
-    }
-  }
-
-  Future getFilePath(String type, String assetPath) async {
-    final directory = await getTemporaryDirectory();
-    return "${directory.path}/fileview/${base64.encode(utf8.encode(assetPath))}.$type";
   }
 }
